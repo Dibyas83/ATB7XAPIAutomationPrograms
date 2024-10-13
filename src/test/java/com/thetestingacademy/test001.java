eb978d1fd9a71852024f1ee317b5ec1539dc0c4c
a2z <dibyas83@gmail.com>
2024-10-13 09:45:47 +0530

    ist

--- src/test/java/com/thetestingacademy/test001.java
+++ src/test/java/com/thetestingacademy/test001.java
@@ -1,2 +1,25 @@
-package com.thetestingacademy;public class test001 {
+package com.thetestingacademy;
+
+import io.restassured.RestAssured;
+
+public class test001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Rest assured test case");
+        System.out.println("Get Request Demo");
+        //gherkins  syntax request
+        // given()-url,headers,body or payload
+        // when() -http methods-get,post,patch,put,delete  body/payload-json,xml,txt,file doc,excel
+        // then()-verify the response - er ==ar
+        RestAssured
+                .given()
+                    .baseUri("https://restful-booker.herokuapp.com")
+                    .basePath("/booking/1").log().all()
+                .when()
+                    .get()
+                .then()
+                .statusCode(201);
+
+
+    }
 }
